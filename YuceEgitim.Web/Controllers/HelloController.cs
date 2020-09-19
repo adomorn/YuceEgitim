045a117d8449545cb3cab4dd23404ab804129b1c
Arda Terekeci <x_anthos@hotmail.com>
2020-09-19 09:02:52 +0300

    Add project files.

---
+++ YuceEgitim.Web/Controllers/HelloController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace YuceEgitim.Web.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            TempData["Title"] = "Merhaba";
+            ViewBag.Title = "Merhaba2";
+            return View();
+        }
+
+        public IActionResult Index3()
+        {
+            TempData["Title"] = "Merhaba";
+            ViewBag.Title = "Merhaba2";
+            return View();
+        }
+    }
+}
---
+++ YuceEgitim.Web/Models/UserModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YuceEgitim.Web.Models
+{
+    public class UserModel
+    {
+    }
+}
